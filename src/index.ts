e99c096fcf5f9bc0f226a2b36999fe0e0d9af5a3
Felipe <fcalldas@gmail.com>
2023-11-16 22:24:51 -0300

    ✨ feat: create a simple server

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { router } from './routes/user.routes';
+
+const app = express();
+dotenv.config();
+const port = 3001;
+
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(
+    `Server running on port ${port}🔥`
+  );
+});
