f541d8b2bacc3ba36f2cbfaf045accee78c7b1a1
Felipe <fcalldas@gmail.com>
2023-11-17 19:42:56 -0300

    🚧 feat: work in progress in user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,17 @@
 import { Router } from 'express';
+import { UserController } from '../controllers/userController';
 
 export const router = Router();
 
-router.get('/', (req, res) => {
-  res.send({message: 'Welcome'});
-});
+const userController = new UserController();
+
+
+router.get('/getAllUsers', userController.getUserById);
+
+router.get('/getUser/:id', userController.getUserById);
+
+router.post('/createUser', userController.createUser);
+
+router.put('/updateUser/:id', userController.updateUser);
+
+router.delete('/deleteUser/:id', userController.deleteUser);
