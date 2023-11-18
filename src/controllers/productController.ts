2bcbb30069d6de960c2bae559dafd1369028603a
Felipe <fcalldas@gmail.com>
2023-11-18 11:04:39 -0300

    🚧 feat: work in progress in productController

---
+++ src/controllers/productController.ts
@@ -0,0 +1,38 @@
+export class ProductController {
+
+  async getProductById(req: any, res: any){
+    res.status(200).json('Get ProductById')
+  }
+
+  async getAllProducts(req: any, res: any) {
+    try {
+      res.status(200).json('Get all Products')
+    } catch (error) {
+      res.status(500).json('Some error occurred in GetAllProducts')      
+    }
+  }
+
+  async createProduct(req: any, res: any){
+    try {
+      res.status(200).json('Product Create with Success')
+    } catch (error) {
+      res.status(500).json('Some error occurred in createProduct')
+    }
+  }
+
+  async updateProduct(req: any, res: any){
+    try {
+      res.status(200).json('Product Update with Success')
+    } catch (error) {
+      res.status(500).json('Some error occurred in updateProduct')
+    }
+  }
+
+  async deleteProduct(req: any, res: any){
+    try {
+      res.status(200).json('Product Deleted with Success')
+    } catch (error) {
+      res.status(500).json('Some error occurred in deleteProduct')
+    }
+  }
+}
