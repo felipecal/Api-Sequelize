5be34b0fbdfe23ca38e32b5732f495ea35c2d515
Felipe <fcalldas@gmail.com>
2023-11-22 19:13:42 -0300

    🚧 feat: work in progress in userService

---
+++ src/services/userService.ts
@@ -0,0 +1,22 @@
+export class UserService {
+  async getAllUsers(req: any, res: any) {
+
+  }
+
+
+  async getUserById(req: any, res: any) {
+
+  }
+
+  async createUser(req: any, res: any) {
+
+  }
+
+  async updateUser(req: any, res: any) {
+
+  }
+
+  async deleteUserById(req: any, res: any) {
+
+  }
+}
