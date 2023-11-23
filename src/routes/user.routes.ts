1590262ace72fbdf4c33bf0272456aaada1f1ee6
Felipe <fcalldas@gmail.com>
2023-11-23 19:53:15 -0300

    🐛 bug: fix a bug in routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,17 +1,15 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/userController';
-import { UserService } from '../services/userService';
 
 export const router = Router();
-const userService = new UserService();
-const userController = new UserController(userService);
+const userController = new UserController();
 
-router.get('/getAllUsers', userController.getAllUsers.bind(userController));
+router.get('/getAllUsers', (req, res) => userController.getAllUsers(req, res));
 
-router.get('/getUser/:id', userController.getUserById.bind(userController));
+router.get('/getUser/:id', (req, res) => userController.getUserById(req, res));
 
-router.post('/createUser', userController.createUser.bind(userController));
+router.post('/createUser', (req, res) => userController.createUser(req, res));
 
-router.put('/updateUser/:id', userController.updateUser.bind(userController));
+router.put('/updateUser/:id', (req, res) => userController.updateUser(req, res));
 
-router.delete('/deleteUser/:id', userController.deleteUser.bind(userController));
+router.delete('/deleteUser/:id', (req, res) => userController.deleteUser(req, res));
