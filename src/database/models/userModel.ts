5571b8a208a1ddd70d4a747fca094de71dc02941
Felipe <fcalldas@gmail.com>
2023-11-26 20:09:23 -0300

    ✨ feat: add user model

---
+++ src/database/models/userModel.ts
@@ -0,0 +1,30 @@
+import { sequelize } from "../../config/config";
+import { Model } from "sequelize";
+import { DataType } from "sequelize-typescript";
+
+export class UserModel extends Model {
+
+}
+
+UserModel.init({
+  user_id: {
+    type: DataType.INTEGER,
+    primaryKey: true
+  },
+  user_name: {
+    type: DataType.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataType.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataType.STRING,
+    allowNull: false
+  }
+}, {
+  tableName: "users",
+  timestamps:  false,
+  sequelize
+})
