a6609d7a8f1a800962e1d992d22f9a1dd8f39030
Felipe <fcalldas@gmail.com>
2023-12-03 14:08:20 -0300

    ✨ feat: creating product model

---
+++ src/database/models/productModel.ts
@@ -0,0 +1,37 @@
+import { sequelize } from '../../config/config';
+import { Model } from 'sequelize';
+import { UserModel } from './userModel';
+import { DataType } from 'sequelize-typescript';
+
+export class ProductModel extends Model { }
+
+ProductModel.init(
+  {
+    product_id: {
+      type: DataType.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataType.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataType.STRING,
+      allowNull: false,
+    },
+    cod_user: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      references: {
+        model: UserModel,
+        key: 'user_id',
+      },
+    },
+  },
+  {
+    tableName: 'products',
+    timestamps: false,
+    sequelize,
+  },
+);
