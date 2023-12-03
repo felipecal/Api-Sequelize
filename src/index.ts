7ea8b15f40b6b736ce3a05a798033a1e903d5648
Felipe <fcalldas@gmail.com>
2023-12-03 14:07:38 -0300

    🚚 feat: add new product route

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { router } from './routes/user.routes';
+import { userRoutes } from './routes/user.routes';
+import { procutRoutes } from './routes/product.routes';
 
 export const app = express();
 dotenv.config();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(router);
+app.use(userRoutes, procutRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}🔥`);
