b6ea66c71c2c9d5b3007ac9bca71d0c9ed19df24
Felipe <fcalldas@gmail.com>
2023-12-03 14:09:07 -0300

    ♻️ refactor: change the name of exports

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/userController';
 
-export const router = Router();
+export const userRoutes = Router();
 const userController = new UserController();
 
-router.get('/getAllUsers', (req, res) => userController.getAllUsers(req, res));
+userRoutes.get('/getAllUsers', (req, res) => userController.getAllUsers(req, res));
 
-router.get('/getUser/:id', (req, res) => userController.getUserById(req, res));
+userRoutes.get('/getUser/:id', (req, res) => userController.getUserById(req, res));
 
-router.post('/createUser', (req, res) => userController.createUser(req, res));
+userRoutes.post('/createUser', (req, res) => userController.createUser(req, res));
 
-router.put('/updateUser/:id', (req, res) => userController.updateUser(req, res));
+userRoutes.put('/updateUser/:id', (req, res) => userController.updateUser(req, res));
 
-router.delete('/deleteUser/:id', (req, res) => userController.deleteUser(req, res));
+userRoutes.delete('/deleteUser/:id', (req, res) => userController.deleteUser(req, res));
