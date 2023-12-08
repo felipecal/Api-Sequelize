1be05521404b9cee9c4f615beea9915116cc5d85
Felipe <fcalldas@gmail.com>
2023-12-07 21:54:40 -0300

    ✨ feat: create database Connection

--- src/config/config.ts
+++ src/config/config.ts
@@ -16,4 +16,14 @@ export const sequelize = new Sequelize({
     timestamps: true,
     underscored: true,
   },
-}); //TODO: Create database connection function
+});
+
+
+export function databaseConnection() {
+  try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
