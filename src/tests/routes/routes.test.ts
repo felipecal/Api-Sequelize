c7269fa935c8ee7e7ad67ead4992c5ed352620e6
Felipe <fcalldas@gmail.com>
2023-12-12 16:53:15 -0300

    🎨 refactor: format the code

--- src/tests/routes/routes.test.ts
+++ src/tests/routes/routes.test.ts
@@ -1,5 +1,3 @@
-
-
 import request from 'supertest';
 import { app } from '../../index';
 import { describe, expect } from '@jest/globals';
@@ -18,18 +16,18 @@ describe('Should routes routes', () => {
 
     it('Should create a user with success', async () => {
       const res = await request(app).post('/createUser').send({
-        user_name: "Teste",
-        password: "Teste1234",
-        email: "teste@teste.com"
+        user_name: 'Teste',
+        password: 'Teste1234',
+        email: 'teste@teste.com',
       });
       expect(res.status).toBe(200);
     });
 
     it('Should update a user with success', async () => {
       const res = await request(app).put('/updateUser/1').send({
-        user_name: "TesteUpdate",
-        password: "Teste1234",
-        email: "teste@teste.com"
+        user_name: 'TesteUpdate',
+        password: 'Teste1234',
+        email: 'teste@teste.com',
       });
       expect(res.status).toBe(200);
     });
@@ -38,7 +36,6 @@ describe('Should routes routes', () => {
       const res = await request(app).delete('/deleteUser/2');
       expect(res.status).toBe(200);
     });
-
   }),
     describe('Should teste User routes with success', () => {
       it('Should get all products with success', async () => {
@@ -53,17 +50,18 @@ describe('Should routes routes', () => {
 
       it('Should create a product with success', async () => {
         const res = await request(app).post('/createProduct').send({
-          product_name: "Ryzen 7 7500X",
-          description: "Quando você tem a arquitetura de processador de desktop mais avançada do mundo para jogadores e criadores de conteúdo, as possibilidades são infinitas.",
-          value: 1200.00,
-          cod_user: 1
+          product_name: 'Ryzen 7 7500X',
+          description:
+            'Quando você tem a arquitetura de processador de desktop mais avançada do mundo para jogadores e criadores de conteúdo, as possibilidades são infinitas.',
+          value: 1200.0,
+          cod_user: 1,
         });
         expect(res.status).toBe(200);
       });
 
       it('Should update a product with success', async () => {
         const res = await request(app).put('/updateProduct/1').send({
-          value: 12000.00
+          value: 12000.0,
         });
         expect(res.status).toBe(200);
       });
@@ -72,5 +70,5 @@ describe('Should routes routes', () => {
         const res = await request(app).delete('/deleteProduct/2');
         expect(res.status).toBe(200);
       });
-    })
+    });
 });
