fa1356a5b4965ddb85506ed0bf6cc8b02368b08d
Felipe <fcalldas@gmail.com>
2023-12-25 21:02:15 -0300

    📦 feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { userRoutes } from './routes/user.routes';
 import { procutRoutes } from './routes/product.routes';
+import cors from "cors";
 
 export const app = express();
 dotenv.config();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(userRoutes, procutRoutes);
 
