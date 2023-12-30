3f7634ff7c99f2db6fbbdf0056334e3d94673eaf
Felipe <fcalldas@gmail.com>
2023-12-30 18:48:59 -0300

    🚚 feat:add new routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -8,6 +8,8 @@ userRoutes.get('/getAllUsers', (req, res) => userController.getAllUsers(req, res
 
 userRoutes.get('/getUser/:id', (req, res) => userController.getUserById(req, res));
 
+userRoutes.get('/getUserByUsername/:username/:password', (req, res) => userController.getUserByUsername(req, res));
+
 userRoutes.post('/createUser', (req, res) => userController.createUser(req, res));
 
 userRoutes.put('/updateUser/:id', (req, res) => userController.updateUser(req, res));
