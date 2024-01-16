585fd33ba6496345a8228fef5549ba8f19d20f61
Felipe <fcalldas@gmail.com>
2024-01-16 15:47:23 -0300

    🎨 refactor: improve structure of the code

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -2,40 +2,54 @@ import { ProductModel } from '../models/productModel';
 
 export class ProductService {
   async getAllProducts() {
-    const productResult = await ProductModel.findAll();
-    return productResult;
+    try {
+      const productResult = await ProductModel.findAll();
+      return productResult;
+    } catch (error) {
+      console.error(`Some error ocurred in getAllProducts ${error}`)
+    }
   }
 
   async getProductById(req: any) {
-    const productId = req.params.id;
-    const productResult = await ProductModel.findByPk(productId);
-    return productResult;
+    try {
+      const productId = req.params.id;
+      const productResult = await ProductModel.findByPk(productId);
+      return productResult;
+    } catch (error) {
+      console.error(`Some error ocurred in getProductById ${error}`)
+    }
   }
 
   async createProduct(req: any) {
-    const productBody = req.body;
-    if (!productBody) {
-      console.log('Cannot create the data, because the body is not passed');
+    try {
+      const productBody = req.body;
+      if (!productBody) throw new Error('Cannot create the data, because the body is not passed');
+      const productResult = await ProductModel.create({
+        product_name: productBody.product_name,
+        description: productBody.description,
+        value: productBody.value,
+        cod_user: productBody.cod_user,
+      });
+      return productResult;
+    } catch (error) {
+      console.error(`Some error ocurred in createProduct ${error}`);
     }
-    const productResult = await ProductModel.create({
-      product_name: productBody.product_name,
-      description: productBody.description,
-      value: productBody.value,
-      cod_user: productBody.cod_user,
-    });
-    return productResult;
   }
 
   async updateProduct(req: any) {
-    const productId = req.params.id;
-    const body = req.body;
-    const product = await ProductModel.findByPk(productId);
-    if (!product) throw new Error('Product not found');
-    const resultOfUpdateUser = await ProductModel.update(body, {
-      where: { product_id: productId },
-      returning: true,
-    });
-    return resultOfUpdateUser;
+    try {
+      const productId = req.params.id;
+      const body = req.body;
+      const product = await ProductModel.findByPk(productId);
+      if (!product) throw new Error('Product not found');
+      const resultOfUpdateUser = await ProductModel.update(body, {
+        where: { product_id: productId },
+        returning: true,
+      });
+      return resultOfUpdateUser;
+    } catch (error) {
+      console.error(`Some error ocurred in updatePorudct ${error}`)
+    }
   }
 
   async delteProduct(req: any) {
@@ -45,7 +59,7 @@ export class ProductService {
       const deleteProduct = await getProduct.destroy();
       return deleteProduct;
     } else {
-      console.log(`Product with id ${productId} was not found`);
+      console.error(`Product with id ${productId} was not found`);
     }
   }
 }
