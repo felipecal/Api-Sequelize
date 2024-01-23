fd8b6d5b9bcfeecec46a17420b76918680bbd8c4
Felipe <fcalldas@gmail.com>
2024-01-23 20:11:19 -0300

    🚧  feat: work in progress about swagger

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,34 @@ import dotenv from 'dotenv';
 import { userRoutes } from './routes/user.routes';
 import { procutRoutes } from './routes/product.routes';
 import cors from 'cors';
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
 
 export const app = express();
 dotenv.config();
 const port = process.env.PORT || 3001;
 
+const swaggerDefinition = {
+  info: {
+    title: 'Api Sequelize',
+    version: "1.0",
+    description: "Documenation of the Sequelize Api"
+  },
+  components: {
+    schemas: require('./schemas.json')
+  }
+};
+const options = {
+  swaggerDefinition: swaggerDefinition,
+  apis: ['./routes/*.ts']
+}
+
 app.use(cors());
 app.use(express.json());
 app.use(userRoutes, procutRoutes);
+
+const swaggerSpec = swaggerJsDoc(options);
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}🔥`);
