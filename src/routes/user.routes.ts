205d926b27805ee45808e20ae87795580dfbb0f2
Felipe <fcalldas@gmail.com>
2024-01-24 18:58:28 -0300

    ♻️ refactor: creating routes doc

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,7 +4,46 @@ import { UserController } from '../controllers/userController';
 export const userRoutes = Router();
 const userController = new UserController();
 
+/**
+ * @swagger
+ * /getAllUsers:
+ *   get:
+ *     tags: 
+ *      - Usuario
+ *     description: Retrieve a list of all users in the application.
+ *     produces: 
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         schema:
+ *           $ref: '#/components/schemas/Usuario'
+ */
+
 userRoutes.get('/getAllUsers', (req, res) => userController.getAllUsers(req, res));
+
+
+/**
+ * @swagger
+ * /getUser/:id:
+ *   get:
+ *     tags: 
+ *      - Usuário
+   *     description: Get a user by id.
+ *     produces: 
+ *       - application/json
+ *     parameters: 
+ *      - in: path
+ *        id: id
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         schema:
+ *           $ref: '#/components/schemas/Usuario'
+ *      400: 
+ *         description: User was not found
+ */
 
 userRoutes.get('/getUser/:id', (req, res) => userController.getUserById(req, res));
 
