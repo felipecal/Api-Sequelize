650475e6ac4b1ff40cfde95c9c1a935aafb3517c
Felipe <fcalldas@gmail.com>
2024-01-25 16:56:19 -0300

    🚧 feat: work in progress about swagger files

---
+++ src/swagger/userSwagger.js
@@ -0,0 +1,37 @@
+/**
+ * @swagger
+ * /getAllUsers:
+ *   get:
+ *     tags:
+ *      - User
+ *     description: Retrieve a list of all users in the application.
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         schema:
+ *           $ref: '#/components/schemas/User'
+ */
+
+/**
+ * @swagger
+ * /getUser/:id:
+ *   get:
+ *     tags:
+ *      - User
+ *     description: Get a user by id.
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *      - in: path
+ *        id: id
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         schema:
+ *           $ref: '#/components/schemas/User'
+ *      400:
+ *         description: User was not found
+ */
