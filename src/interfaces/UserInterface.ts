93ae27311bc53c9d4b62fdccc61bf031d381d55a
Felipe <fcalldas@gmail.com>
2024-02-05 21:02:35 -0300

    🏷️ feat: add new interface

---
+++ src/interfaces/UserInterface.ts
@@ -0,0 +1,25 @@
+import { UserModel } from "../models/userModel";
+
+export interface CreateUser {
+  status?: {
+    statusOfUser: string;
+  };
+  userResult?: undefined;
+  updateUserResult?: undefined;
+  error?: string;
+}
+
+export interface User {
+  user_id?: number;
+  user_name?: string;
+  password?: string;
+  email?: string;
+  error?: string;
+}
+
+export interface UpdateUser {
+  message?: string;
+  content?: [number, UserModel[]] | { error: string };
+  error?: string;
+}
+
