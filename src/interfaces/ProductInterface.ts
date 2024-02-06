0eebd336f52a30bcf885ed228cea6ff45dfb5362
Felipe <fcalldas@gmail.com>
2024-02-06 20:36:04 -0300

    🏷️ feat: add new interface

---
+++ src/interfaces/ProductInterface.ts
@@ -0,0 +1,17 @@
+import { ProductModel } from "../models/productModel";
+
+export interface Product {
+  product_id?: number;
+  product_name?: string;
+  description?: string;
+  value?: number;
+  quantity?: number;
+  cod_user?: number;
+  error?: string;
+}
+
+export interface UpdateProduct {
+  message?: string;
+  content?: [number, ProductModel[] | { error: string }];
+  error?: string;
+}
