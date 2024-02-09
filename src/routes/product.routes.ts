0855580c57564bc2c293a89236961cc4cd863ca7
Felipe <fcalldas@gmail.com>
2024-02-09 18:36:56 -0300

    ✨ feat: add middleware in routes

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,16 +1,16 @@
 import { Request, Response, Router } from 'express';
 import { ProductController } from '../controllers/productController';
+import { authMiddleware } from '../middlewares/authMiddleware';
 
 export const productsRoutes = Router();
-
 const productController = new ProductController();
 
-productsRoutes.get('/getAllProducts', (req: Request, res: Response) => productController.getAllProducts(res));
+productsRoutes.get('/getAllProducts', authMiddleware, (req: Request, res: Response) => productController.getAllProducts(req, res));
 
 productsRoutes.get('/getProduct/:id', (req: Request, res: Response) => productController.getProductById(req, res));
 
-productsRoutes.post('/createProduct', (req: Request, res: Response) => productController.createProduct(req, res));
+productsRoutes.post('/createProduct', authMiddleware, (req: Request, res: Response) => productController.createProduct(req, res));
 
-productsRoutes.put('/updateProduct/:id', (req: Request, res: Response) => productController.updateProduct(req, res));
+productsRoutes.put('/updateProduct/:id', authMiddleware, (req: Request, res: Response) => productController.updateProduct(req, res));
 
 productsRoutes.delete('/deleteProduct/:id', (req: Request, res: Response) => productController.deleteProduct(req, res));
