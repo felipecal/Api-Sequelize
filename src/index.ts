7b246f9640ec1c8237ac2e7ed8a4c70e6733a57d
Felipe <fcalldas@gmail.com>
2024-02-16 18:45:51 -0300

    ✨ feat: add token to swagger config

--- src/index.ts
+++ src/index.ts
@@ -18,13 +18,25 @@ const swaggerDefinition = {
     description: 'Documentation of the Sequelize API',
   },
   components: {
+    securitySchemes: {
+      BearerAuth: {
+        type: 'apiKey',
+        name: 'Authorization',
+        in: 'header',
+      },
+    },
     schemas: { User: require('./swagger/userSchema.json'), Product: require('./swagger/productSchema.json') },
+    security: [
+      {
+        BearerAuth: [],
+      },
+    ],
   },
 };
 
 const options = {
   swaggerDefinition,
-  apis: [__dirname + '/swagger/userSwagger.js', __dirname + '/swagger/productSwagger.js'],
+  apis: [__dirname + '/swagger/userSwagger.js', __dirname + '/swagger/productSwagger.js', __dirname + '/routes/*.ts'],
 };
 
 const swaggerSpec = swaggerJsDoc(options);
