2b9651356559b030917757368d63bafdb0f9640b
Felipe <fcalldas@gmail.com>
2024-02-27 22:01:50 -0300

    🔧 refactor: change default value env

--- sequelize.js
+++ sequelize.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
-const host = process.env.DB_HOST || '127.0.0.1';
+const host = process.env.DB_HOST || 'postgres';
 const port = parseInt(process.env.DB_PORT || 5432);
 const username = process.env.DB_USERNAME || 'postgres';
 const password = process.env.DB_PASSWORD || '1234';
-const database = process.env.DB_DATABASE || 'users';
+const database = process.env.DB_DATABASE || 'postgres';
 const dialect = 'postgres';
 
 module.exports = {
